fab63d8ddef9121818c22c3b282754daf0481751
Ryan Bueter <magnus010@bueters.com>
2017-09-03 11:48:39 -0600

    Inserted output statements. Updated path management. Removed prior converted files.

--- mdi2pdf.py
+++ mdi2pdf.py
@@ -12,26 +12,36 @@ def mdi2tif_function(__file_name__):
     path = os.path.dirname(__file_name__)
     log_parameter = "-log"
     log_file_name = "mdi2tif.log"
+    if os.path.isfile(os.path.join(path, new_file_name)):
+        print("Removing \'" + new_file_name + "\'")
+        os.remove(os.path.join(path, new_file_name))
     print("Converting \'" + old_file_name + "\' to \'" + new_file_name + "\'")
     output = Popen([mdi2tif, source_parameter, os.path.join(path, old_file_name), log_parameter,
                     log_file_name, dest_parameter, os.path.join(path, new_file_name)], stdout=PIPE)
-    print output.stdout.read()
+    print(output.stdout.read())
 
 
 def tif2pdf_function(__file_name__):
     tif2pdf = "C:/Program Files/ImageMagick-7.0.6-Q16/magick.exe"
 
+    old_path = os.getcwd()
     os.chdir(os.path.dirname(tif2pdf))
 
     old_file_name = os.path.basename(os.path.splitext(__file_name__)[0]) + '.tif'
-    new_file_name = os.path.basename(os.path.splitext(__file_name__)[0]) + '.pdf'
+    new_file_name = os.path.basename(os.path.splitext(__file_name__)[0]) + '-conv.pdf'
     path = os.path.dirname(__file_name__)
+    if os.path.isfile(os.path.join(path, new_file_name)):
+        print("Removing \'" + new_file_name + "\'")
+        os.remove(os.path.join(path, new_file_name))
     print("Converting \'" + old_file_name + "\' to \'" + new_file_name + "\'")
     output = Popen([tif2pdf, os.path.join(path, old_file_name), os.path.join(path, new_file_name)], stdout=PIPE)
-    print output.stdout.read()
+    print(output.stdout.read())
+    print("Removing \'" + old_file_name + "\'")
+    os.remove(os.path.join(path, old_file_name))
+    os.chdir(old_path)
 
 if __name__ == "__main__":
-    home_folder = "c:/test"
+    home_folder = os.getcwd()
     file_list = []
 
     for root, dirs, files in os.walk(home_folder):
